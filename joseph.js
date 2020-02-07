ceb0205ea8f1642ab2d6fad6acc3ed49d664150d
Gus2323 <gustavocastaneda2323@gmail.com>
2020-02-07 23:49:54 +0000

    update

--- joseph.js
+++ joseph.js
@@ -2,13 +2,14 @@ $("#dish").click(function(){
  $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
  $(".answer2").text("2) Many employers won't consider unemployed job applicants. Because you are unemployed you did not get the job");
  $(".answer3").text("3) Many homeless people don't have reliable phones. Since you don't have access to a phone the employer could not contact you");
+$(".answer4").text("Homeless people encounter many barriers. They have difficulty accessing the things they need. A solution to this would be giving them easier access to resources.");
 
 });
-
 
 $(".jan").click(function(){
  $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
  $(".answer2").text("2) Many employers won't consider unemployed job applicants. Because you are unemployed you did not get the job");
  $(".answer3").text("3) Many homeless people don't have reliable phones. Since you don't have access to a phone the employer could not contact you");
+$(".answer4").text("Homeless people encounter many barriers. They have difficulty accessing the things they need. A solution to this would be giving them easier access to resources.");
 
 });
