7713dacb3fe0b81f1285220206fabf224eaf68f8
Gus2323 <gustavocastaneda2323@gmail.com>
2020-02-07 23:40:43 +0000

    update

--- joseph.js
+++ joseph.js
@@ -1,16 +1,13 @@
 $("#dish").click(function(){
- $(".answer").text("Most employers require addresses. Because you are homeless the employer did not hire you");
+ $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
+ $(".answer2").text("2) Many employers won't consider unemployed job applicants.");
+ $(".answer3").text("3) Many homeless people don't have reliable phones");
+
 });
+
 $(".jan").click(function(){
- $(".answer").text("Most employers require addresses. Because you are homeless the employer did not hire you");
-});
-$(function(){
-    $('#dish').on('click',function(){
-        var r= $('<input type="button" value="Next"/>');
-        $("body").append(r);
-    });
-    $('.jan').on('click',function(){
-        var r= $('<input type="button" value="Next"/>');
-        $("body").append(r);
-    });
+ $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
+ $(".answer2").text("2) Many employers won't consider unemployed job applicants.");
+ $(".answer3").text("3) Many homeless people don't have reliable phones");
+
 });
