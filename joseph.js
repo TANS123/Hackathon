64addd7488a50d0b5afc5d79c49627df79c4cdda
Gus2323 <gustavocastaneda2323@gmail.com>
2020-02-07 23:42:33 +0000

    update

--- joseph.js
+++ joseph.js
@@ -1,13 +1,14 @@
 $("#dish").click(function(){
  $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
- $(".answer2").text("2) Many employers won't consider unemployed job applicants.");
- $(".answer3").text("3) Many homeless people don't have reliable phones");
+ $(".answer2").text("2) Many employers won't consider unemployed job applicants. Because you are unemployed you did not get the job");
+ $(".answer3").text("3) Many homeless people don't have reliable phones. Since you don't have access to a phone the employer could not contact you");
 
 });
 
+
 $(".jan").click(function(){
  $(".answer").text("1) Most employers require addresses. Because you are homeless the employer did not hire you. Many employers won't consider unemployed job applicants. Many homeless people don't have reliable phones");
- $(".answer2").text("2) Many employers won't consider unemployed job applicants.");
- $(".answer3").text("3) Many homeless people don't have reliable phones");
+ $(".answer2").text("2) Many employers won't consider unemployed job applicants. Because you are unemployed you did not get the job");
+ $(".answer3").text("3) Many homeless people don't have reliable phones. Since you don't have access to a phone the employer could not contact you");
 
 });
