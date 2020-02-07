380f7e92229c60251402f5123e26a1da80c78511
Gus2323 <gustavocastaneda2323@gmail.com>
2020-02-07 23:28:27 +0000

    update

--- joseph.js
+++ joseph.js
@@ -1,4 +1,16 @@
 $("#dish").click(function(){
-  console.log("clicked");
- $(".answer").text("Hello world!");
+ $(".answer").text("Most employers require addresses. Because you are homeless the employer did not hire you");
+});
+$(".jan").click(function(){
+ $(".answer").text("Most employers require addresses. Because you are homeless the employer did not hire you");
+});
+$(function(){
+    $('#dish').on('click',function(){
+        var r= $('<input type="button" value="Next"/>');
+        $("body").append(r);
+    });
+    $('.jan').on('click',function(){
+        var r= $('<input type="button" value="Next"/>');
+        $("body").append(r);
+    });
 });
